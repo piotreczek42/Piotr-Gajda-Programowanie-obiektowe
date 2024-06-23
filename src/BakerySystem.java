e137d4ef1ce9e27f86b711b45ab371695320168d
piotr <piotrek__gajda@o2.pl>
2024-06-23 12:57:55 +0200

    123

---
+++ src/BakerySystem.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BakerySystem extends JFrame {
+    private JPanel panel1;
+    private JTextField textFieldNazwisko;
+    private JTextField textFieldImie;
+    private JButton btnZamow;
+    private JButton btnSkladniki;
+    private JLabel lblNazwisko;
+    private JLabel lblImie;
+    private JLabel lblKlient;
+    private JLabel lblProdukt;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JPanel JPanelPiekarnia;
+
+    public BakerySystem() {
+        btnZamow.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnSkladniki.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BakerySystem gui = new BakerySystem();
+        gui.show();
+    }
+}
